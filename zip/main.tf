90d960f471a27947c760f16b09fa21ed563d1753
Martin Panelati <martin.panelati@personio.de>
2025-08-24 11:15:52 +0200

    try A2

--- zip/main.tf
+++ zip/main.tf
@@ -111,7 +111,7 @@ resource "oci_core_instance" "n8n_instance" {
   availability_domain = data.oci_identity_availability_domains.ads.availability_domains[0].name
   compartment_id      = local.compartment_ocid
   display_name        = "n8n-instance"
-  shape               = "VM.Standard.A1.Flex"
+  shape               = "VM.Standard.A2.Flex"
 
   shape_config {
     memory_in_gbs = 4
