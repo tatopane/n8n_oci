b02648d6f00a4b7fab9a1855b41f8c5f54a8a4a4
Martin Panelati <martin.panelati@personio.de>
2025-08-24 08:42:17 +0200

    add zip for deploy

---
+++ zip/variables.tf
@@ -0,0 +1,10 @@
+variable "tenancy_ocid" {
+  description = "OCID of Oracle Cloud Tenancy"
+  type        = string
+}
+
+variable "ssh_public_key" {
+  description = "SSH public key"
+  type        = string
+}
+
